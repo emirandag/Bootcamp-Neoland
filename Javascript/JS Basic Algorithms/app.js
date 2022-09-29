73da2c05ef9ea79299f90bb4e8b104b1c61cf6f9
emirandag <edulucho88@gmail.com>
2022-09-29 19:58:42 +0200

    Iteration#1

---
+++ Javascript/JS Basic Algorithms/app.js
@@ -0,0 +1,17 @@
+/**
+ * --------------------Iteración #1: Variables------------------
+ */
+//1.1
+let myFavoriteHero = "Hulk";
+console.log(myFavoriteHero);
+
+//1.2
+let x = 50;
+ 
+//1.3
+let h = 5;
+let y = 10;
+
+//1.4
+let z = h + y;
+console.log(z);
