63bf2b49671088fe83021f179430f26070db6e6b
emirandag <edulucho88@gmail.com>
2022-09-30 20:28:33 +0200

    Iteration#3

--- Javascript/JS Functions/app.js
+++ Javascript/JS Functions/app.js
@@ -10,3 +10,30 @@
   }
 
 sum(10, 8);
+
+
+
+/**
+ * --------------------Iteración #2: Buscar la palabra más larga------------------
+ */
+ const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+ function findLongestWord(param) {
+
+ }
+
+
+
+ /**
+ * --------------------Iteración #3: Calcular la suma------------------
+ */
+  const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+  let total = 0;  
+  function sumAll(array) {
+    for (let i = 0; i < array.length; i++) {
+        total += array[i];     
+    }
+    console.log(total);
+  }
+
+  sumAll(numbers);
