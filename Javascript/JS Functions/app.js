f1ba39c53c29a9f7069dcc43d1680582e7a5a100
emirandag <edulucho88@gmail.com>
2022-09-30 19:57:35 +0200

    Iteration#1

---
+++ Javascript/JS Functions/app.js
@@ -0,0 +1,12 @@
+/**
+ * --------------------Iteración #1: Buscar el máximo------------------
+ */
+ function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo) {
+        console.log("El más alto es: " + numberOne);
+    } else {
+        console.log("El más alto es: " + numberTwo);
+    }
+  }
+
+sum(10, 8);
