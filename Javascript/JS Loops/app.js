ac7f036af2f3157373e66b939400c7cd6a93bd94
emirandag <edulucho88@gmail.com>
2022-10-01 22:45:43 +0200

    Iteration#2

--- Javascript/JS Loops/app.js
+++ Javascript/JS Loops/app.js
@@ -15,3 +15,33 @@ for (let i = 0; i < products.length; i++) {
 }
 
 console.log("-----FIN I#1------");
+
+
+/**
+* --------------------Iteración #2: Condicionales avanzados------------------
+*/
+console.log("-----Iteración #2: Condicionales avanzados-----");
+
+const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+
+for (const key in alumns) {
+    
+    const element = alumns[key];
+
+    if ((element.T1 && element.T2) == true || (element.T1 && element.T3) == true || (element.T2 && element.T3)) {
+        element.isApproved = true;
+        console.log(element);
+    } else {
+        element.isApproved = false;
+        console.log(element);
+    }
+}
+
+console.log("-----FIN I#2------");
