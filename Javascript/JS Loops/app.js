31640296c6c793b6bb4fbc42c65a406af651d604
emirandag <edulucho88@gmail.com>
2022-10-01 23:16:11 +0200

    Iteration#4

--- Javascript/JS Loops/app.js
+++ Javascript/JS Loops/app.js
@@ -73,3 +73,24 @@ for (const place of placesToTravel) {
 }
 
 console.log("-----FIN I#3------\n");
+
+
+/**
+ * --------------------Iteración #4: Probando For...in------------------
+ */
+ console.log("-----Iteración #4: Probando For...in-----");
+
+ const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+}
+
+for (const key in alien) {
+
+    console.log(`${key}: ${alien[key]}`);
+
+}
+
+ console.log("-----FIN I#4------\n");
