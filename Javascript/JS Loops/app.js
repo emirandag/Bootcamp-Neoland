0825f1895c8293bf84db9ed1382523c1b267ae0c
emirandag <edulucho88@gmail.com>
2022-10-05 23:02:57 +0200

    Iteration#7

--- Javascript/JS Loops/app.js
+++ Javascript/JS Loops/app.js
@@ -160,3 +160,28 @@ for (const toy of toys) {
 console.log(toysWithoutWordCat);
 
 console.log("-----FIN I#6------\n");
+
+
+
+console.log("-----Iteración #7: For...of avanzado-----");
+
+const popularToys = [];
+const toys2 = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+];
+
+for (const toy of toys2) {
+    console.log(toy.sellCount > 15);
+
+    if (toy.sellCount > 15) {
+        popularToys.push(toy);
+    }
+}
+
+console.log(popularToys);
+
+console.log("-----FIN I#7------\n");
