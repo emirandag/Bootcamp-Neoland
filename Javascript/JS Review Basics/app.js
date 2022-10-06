68a98ba942f0da25fab6cbdf712ebd8c1f1b1c7a
emirandag <edulucho88@gmail.com>
2022-10-06 22:22:17 +0200

    Iteration#2

--- Javascript/JS Review Basics/app.js
+++ Javascript/JS Review Basics/app.js
@@ -35,3 +35,85 @@ for (const movie of movies) {
 console.log(moviesCategory);
 
 console.log("-----FIN I#1------\n");
+
+
+
+/**
+* --------------------Iteración #2: Mix Fors------------------
+*/
+console.log("-----Iteración #2: Mix Fors-----");
+
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+];
+
+
+//let sounds = {}
+
+let sumVolumes = 0;
+let lengthVolumes = [];
+
+
+
+for (const user of users) {
+    //console.log(user.favoritesSounds);
+
+
+    let sounds = user.favoritesSounds;
+    
+
+    for (const sound in sounds) {
+
+        let volumes = sounds[sound].volume;
+        
+        sumVolumes += volumes;
+
+        lengthVolumes.push(volumes);
+        
+    }
+}
+
+console.log(`La suma de todos los valores son: ${sumVolumes}`);
+console.log(lengthVolumes.length);
+
+let middleVolumes = sumVolumes / lengthVolumes.length;
+
+console.log(`La media del volumen de todos los sonidos es: ${middleVolumes}`);
+
+console.log("-----FIN I#2------\n");
+
+
+
+/**
+* --------------------Iteración #3: Mix Fors------------------
+*/
+console.log("-----Iteración #3: Mix Fors-----");
+
+console.log("-----FIN I#3------\n");
