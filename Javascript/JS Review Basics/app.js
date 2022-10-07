ccaecd1709fbc7621d21075d5af9eb935c751b67
emirandag <edulucho88@gmail.com>
2022-10-07 19:27:35 +0200

    Iteration#3

--- Javascript/JS Review Basics/app.js
+++ Javascript/JS Review Basics/app.js
@@ -116,4 +116,62 @@ console.log("-----FIN I#2------\n");
 */
 console.log("-----Iteración #3: Mix Fors-----");
 
+const users2 = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+let favorites = []
+
+for (const user of users2) {
+    //console.log(user);
+
+    for (const sound in user.favoritesSounds) {
+        //console.log(!array.includes(sound), sound);
+
+        //console.log(sound);
+        favorites.push(sound);
+
+    }
+
+}
+
+console.log(favorites);
+
+countFavorites = []
+
+favorites.forEach((element => {
+
+    countFavorites[element] = (countFavorites[element] || 0 ) + 1;
+
+}));
+
+console.log(countFavorites);
+
 console.log("-----FIN I#3------\n");
