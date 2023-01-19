ad5e4b7dbb924f90610dc7fa15470b4dbcba73f1
emirandag <edulucho88@gmail.com>
2023-01-19 18:16:15 +0100

    mini-proyecto-2

---
+++ React/mini-proyecto-reactjs-2/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import { CharacterList } from "./components/CharacterList/CharacterList";
+
+
+const App = () => {
+  return <CharacterList />
+};
+
+export default App;
+
