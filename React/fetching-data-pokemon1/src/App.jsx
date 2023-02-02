e5410ad01400c9817394bcf632e4b0b160e89528
emirandag <edulucho88@gmail.com>
2023-02-02 18:22:19 +0100

    first commit fetching-data-pokemon

---
+++ React/fetching-data-pokemon1/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import FetchingInput from './FetchingInput/FetchingInput'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <FetchingInput />
+    </div>
+  )
+}
+
+export default App
---
+++ React/fetching-data-pokemon1/src/FetchingInput/FetchingInput.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+
+export const FetchingInput = () => {
+
+    const [filter, setFilter] = useState('ditto')
+    const [pokemonCollection, setPokemonCollection] = useState([])
+
+    useEffect(() => {
+        const getPokemonFiltered = async () => {
+            const pokemonList = await fetch (`https://pokeapi.co/api/v2/pokemon/${filter}`)
+
+            const pokemonListToJson = await pokemonList.json()
+
+            console.log(pokemonListToJson.name);
+            return {
+                ...pokemonListToJson,
+                name: pokemonListToJson.name,
+                image: pokemonListToJson.sprites.front_shiny
+            }
+        }
+        getPokemonFiltered().then((pokemon) => setPokemonCollection([pokemon]))
+    }, [filter])
+
+    return (
+        <div>
+            <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+
+            <ul>
+                {pokemonCollection.map((pokemon) => {
+                    <li key={pokemon.name}>
+                        <h1>{pokemon.name}</h1>
+                        <img src={pokemon.image} alt={pokemon.name} />
+                    </li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default FetchingInput
