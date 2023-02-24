0fad72d56c4db59842ba901fccb435d3bde131ca
emirandag <edulucho88@gmail.com>
2023-02-24 22:34:54 +0100

    commit IU component

--- React/nasa-reactjs/src/components/Header/Header.jsx
+++ React/nasa-reactjs/src/components/Header/Header.jsx
@@ -2,12 +2,12 @@ import './Header.css'
 const Header = () => {
   return (
     <header className="header">
-      <h2 className="title">NASA API</h2>
       <img
         className="logo"
         src={'https://api.nasa.gov/assets/img/favicons/favicon-192.png'}
         alt="LOGO NASA"
       />
+      <h2 className="title">NASA API</h2>
     </header>
   );
 };
