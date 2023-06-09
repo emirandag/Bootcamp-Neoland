23038ee43d45465f669921aa26bf312074d619b6
emirandag <edulucho88@gmail.com>
2023-06-09 20:14:58 +0200

    Server HTTP example

---
+++ Node/ServerHttp/server.js
@@ -0,0 +1,13 @@
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+
+server.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000');
+});
